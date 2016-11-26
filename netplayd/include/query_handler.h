82955801cfd8a5450e8d48a31668fa9c1646fc9d
Anurag Khandelwal <anuragk@berkeley.edu>
2016-11-26 15:54:14 -0800

    Fix variable name

--- netplayd/include/query_handler.h
+++ netplayd/include/query_handler.h
@@ -16,7 +16,7 @@ class query_handler : virtual public thrift::NetPlayQueryServiceIf {
   void filter(std::set<int64_t>& _return, const std::string& query) {
     // Your implementation goes here
     assert(_return.empty());
-    auto q = query_utils::expression_to_filter_query(handle_, exp);
+    auto q = query_utils::expression_to_filter_query(handle_, query);
 
     fprintf(stderr, "input query: %s\n", query.c_str());
     fprintf(stderr, "parsed query: \n");
