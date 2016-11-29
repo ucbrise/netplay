5d9edd3f16bef0f816c5f8eb203b49b923ab0e74
Anurag Khandelwal <anuragk@berkeley.edu>
2016-11-28 21:28:13 -0800

    Load queries before data

--- bench/filter_bench.cc
+++ bench/filter_bench.cc
@@ -48,9 +48,10 @@ class filter_benchmark {
 
     store_ = new packet_store();
 
+    fprintf(stderr, "Loading queries...\n");
+    load_queries(query_path);
     fprintf(stderr, "Loading data...\n");
     load_data(load_rate, num_pkts);
-    load_queries(query_path);
     fprintf(stderr, "Initialization complete.\n");
   }
 
