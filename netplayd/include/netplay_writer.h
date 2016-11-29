ac169b5849b32270d5405ab03e19c7db873e1fa4
Anurag Khandelwal <anuragk@berkeley.edu>
2016-11-29 15:07:09 -0800

    Fix minor bug in netplay writer

--- netplayd/include/netplay_writer.h
+++ netplayd/include/netplay_writer.h
@@ -53,7 +53,7 @@ class netplay_writer {
     uint64_t epoch = start;
     while (1) {
       uint16_t recv = vport_->recv_pkts(pkts, BATCH_SIZE);
-      handle->insert_pktburst(pkts, recv);
+      handle_->insert_pktburst(pkts, recv);
       rec_pkts_ += recv;
       req_pkts_ += BATCH_SIZE;
 
