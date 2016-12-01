9628eacdc706edf2f733fe62c1c063b1207d0112
Anurag Khandelwal <anuragk@berkeley.edu>
2016-11-30 21:37:37 -0800

    Fix

--- logstore/include/filterresult.h
+++ logstore/include/filterresult.h
@@ -42,10 +42,10 @@ class filter_result {
     }
 
     filter_iterator& operator++() {
-      fprintf(stderr, "++ called\n");
-      while (cur_tok_ != res_->tok_max_ + 1 &&
-             !res_->olog_->is_valid(cur_entry_list_->get(cur_idx_), res_->max_rid_))
+      do {
         advance();
+      } while (cur_tok_ != res_->tok_max_ + 1 &&
+               !res_->olog_->is_valid(cur_entry_list_->get(cur_idx_), res_->max_rid_));
       return *this;
     }
 
