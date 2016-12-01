63699aeeb94ec5d21caa9f48a36c472aa65e4e7c
Anurag Khandelwal <anuragk@berkeley.edu>
2016-12-01 02:32:36 -0800

    Move util methods to netplay_utils

--- netplayd/include/query_planner.h
+++ netplayd/include/query_planner.h
@@ -9,6 +9,7 @@
 #include "query_parser.h"
 #include "expression.h"
 #include "query_plan.h"
+#include "netplay_utils.h"
 
 namespace netplay {
 
@@ -42,7 +43,7 @@ class query_planner {
       clause_plan _cplan;
       _cplan.valid = true;
       _cplan.perform_pkt_filter = false;
-      _cplan.idx_filter = build_index_filter(h, (predicate*) e);
+      _cplan.idx_filter = netplay_utils::build_index_filter(h, (predicate*) e);
       _plan.push_back(_cplan);
     } else if (e->type == expression_type::AND) {
       conjunction* c = (conjunction*) e;
@@ -50,7 +51,7 @@ class query_planner {
       for (expression* child : c->children) {
         if (child->type != expression_type::PREDICATE)
           throw parse_exception("Filter expression not in DNF");
-        _clause.push_back(build_index_filter(h, (predicate*) child));
+        _clause.push_back(netplay_utils::build_index_filter(h, (predicate*) child));
       }
       clause_plan _cplan = build_clause_plan(h, _clause);
       if (_cplan.valid)
@@ -65,7 +66,7 @@ class query_planner {
         for (expression* cchild : c->children) {
           if (cchild->type != expression_type::PREDICATE)
             throw parse_exception("Filter expression not in DNF");
-          _clause.push_back(build_index_filter(h, (predicate*) cchild));
+          _clause.push_back(netplay_utils::build_index_filter(h, (predicate*) cchild));
         }
         clause_plan _cplan = build_clause_plan(h, _clause);
         if (_cplan.valid)
@@ -161,155 +162,6 @@ class query_planner {
     }
 
     return _plan;
-  }
-
-  static index_filter build_index_filter(const packet_store::handle* h,
-                                         const predicate* p) {
-    // TODO: replace this with a map lookup
-    if (p->attr == "src_ip")
-      return ip_filter(h->srcip_idx(), p->op, p->value);
-    else if (p->attr == "dst_ip")
-      return ip_filter(h->dstip_idx(), p->op, p->value);
-    else if (p->attr == "src_port")
-      return port_filter(h->srcport_idx(), p->op, p->value);
-    else if (p->attr == "dst_port")
-      return port_filter(h->dstport_idx(), p->op, p->value);
-    else if (p->attr == "timestamp")
-      return time_filter(h->timestamp_idx(), p->op, p->value);
-    else
-      throw parse_exception("Invaild attribute: " + p->attr);
-  }
-
-  static index_filter ip_filter(const uint32_t index_id, const std::string& op,
-                                const std::string& ip_string) {
-    index_filter f;
-    f.index_id = index_id;
-    if (op == "in") {
-      f.tok_range = ip_range(ip_string);
-    } else if (op == "!in") {
-      f.tok_range = ip_range(ip_string);
-    } else if (op == "==") {
-      uint32_t ip = ip_string_to_uint32(ip_string.c_str());
-      f.tok_range = index_filter::range(ip, ip);
-    } else if (op == "!=") {
-      uint32_t ip = ip_string_to_uint32(ip_string.c_str());
-      f.tok_range = index_filter::range(ip, ip);
-    } else {
-      throw parse_exception("Specify IP ranges with prefix notation");
-    }
-    return f;
-  }
-
-  static index_filter::range ip_range(const std::string& ip_string) {
-    size_t loc = ip_string.find_first_of('/');
-    uint32_t ip = 0;
-    uint32_t prefix = 32;
-
-    if (loc != std::string::npos) {
-      try {
-        prefix = std::stoi(ip_string.substr(loc + 1));
-      } catch (std::exception& e) {
-        throw parse_exception("Malformed IP range: " + ip_string);
-      }
-      ip = ip_string_to_uint32(ip_string.substr(0, loc).c_str());
-    } else {
-      throw parse_exception("Malformed IP range: " + ip_string);
-    }
-
-    return index_filter::range(ip & ip_prefix_mask[prefix],
-                               ip | (~ip_prefix_mask[32 - prefix]));
-  }
-
-  static uint32_t ip_string_to_uint32(const char* ip) {
-    unsigned char tmp[4];
-    sscanf(ip, "%hhu.%hhu.%hhu.%hhu", &tmp[3], &tmp[2], &tmp[1], &tmp[0]);
-    return tmp[0] | tmp[1] << 8 | tmp[2] << 16 | tmp[3] << 24;
-  }
-
-  static index_filter port_filter(const uint32_t index_id, const std::string& op,
-                                  const std::string & port_string) {
-    uint16_t port = 0;
-    try {
-      port = std::stoi(port_string);
-    } catch (std::exception& e) {
-      throw parse_exception("Malformed port string: " + port_string);
-    }
-
-    index_filter f;
-    f.index_id = index_id;
-
-    if (op == "==")
-      f.tok_range = index_filter::range(port, port);
-    else if (op == "!=")
-      f.tok_range = index_filter::range(port, port);
-    else if (op == "<")
-      f.tok_range = index_filter::range(0, port - 1);
-    else if (op == "<=")
-      f.tok_range = index_filter::range(0, port);
-    else if (op == ">")
-      f.tok_range = index_filter::range(port + 1, UINT16_MAX);
-    else if (op == ">=")
-      f.tok_range = index_filter::range(port, UINT16_MAX);
-    else
-      throw parse_exception("Specify port ranges with <,>,<=,>= operators");
-
-    return f;
-  }
-
-  static index_filter time_filter(const uint32_t index_id, const std::string & op,
-                                  const std::string & time_string) {
-    size_t loc = time_string.find("now");
-    uint32_t time = 0;
-    if (loc != std::string::npos) {
-      // Time relative to "now"
-      if (loc != 0)
-        throw parse_exception("Malformed relative time value; format: now[-value]");
-
-      if (time_string.length() == 3) {
-        time = now;
-
-        if (op.find(">") != std::string::npos)
-          throw parse_exception("Cannot see into the future");
-      } else {
-        loc = time_string.find("-");
-        if (loc != std::string::npos && loc == 3) {
-          try {
-            time = now - std::stoi(time_string.substr(4));
-          } catch (std::exception& e) {
-            throw parse_exception("Malformed relative time value; format: now[-value]");
-          }
-        } else {
-          throw parse_exception("Malformed relative time value; format: now[-value]");
-        }
-      }
-    } else {
-      // Absolute time
-      try {
-        time = std::stol(time_string);
-      } catch (std::exception& e) {
-        throw parse_exception("Malformed relative time value; format: now[-value]");
-      }
-    }
-
-    index_filter f;
-    f.index_id = index_id;
-
-    if (op == "==")
-      f.tok_range = index_filter::range(time, time);
-    else if (op == "!=")
-      f.tok_range = index_filter::range(time, time);
-    else if (op == "<")
-      f.tok_range = index_filter::range(0, time - 1);
-    else if (op == "<=")
-      f.tok_range = index_filter::range(0, time);
-    else if (op == ">")
-      f.tok_range = index_filter::range(time + 1, now);
-    else if (op == ">=")
-      f.tok_range = index_filter::range(time, now);
-    else
-      throw parse_exception("Specify time ranges with <,>,<=,>= operators");
-
-    return f;
   }
 };
 
