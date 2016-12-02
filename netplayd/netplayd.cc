6182461995348c610a3ca3d2dd3f79f40fb35fe5
Anurag Khandelwal <anuragk@berkeley.edu>
2016-12-01 18:46:42 -0800

    Fix headers

--- netplayd/netplayd.cc
+++ netplayd/netplayd.cc
@@ -9,7 +9,21 @@
 #include <exception>
 #include <new>
 
+#include <rte_config.h>
 #include <rte_malloc.h>
+#include <rte_ring.h>
+#include <rte_cycles.h>
+#include <rte_timer.h>
+#include <rte_errno.h>
+#include <rte_eth_ring.h>
+#include <rte_ethdev.h>
+#include <rte_eal.h>
+#include <rte_ether.h>
+#include <rte_ip.h>
+#include <rte_tcp.h>
+#include <rte_udp.h>
+#include <rte_lpm.h>
+#include <rte_mbuf.h>
 
 #include "dpdk_utils.h"
 #include "ovs_init.h"
