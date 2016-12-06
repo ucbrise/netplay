1c368f5bdbf00733c1cff2705537e5152d7fd0c9
Anurag Khandelwal <anuragk@berkeley.edu>
2016-12-05 18:26:06 -0800

    Print stack trace

--- bench/filter_bench.cc
+++ bench/filter_bench.cc
@@ -13,6 +13,12 @@
 #include <sstream>
 #include <algorithm>
 
+#include <execinfo.h>
+#include <signal.h>
+#include <string.h>
+#include <ucontext.h>
+#include <unistd.h>
+
 #include <rte_config.h>
 #include <rte_malloc.h>
 #include <rte_ring.h>
@@ -48,6 +54,14 @@ using namespace ::netplay::dpdk;
 using namespace ::netplay::pktgen;
 using namespace ::slog;
 using namespace ::std::chrono;
+
+typedef struct _sig_ucontext {
+ unsigned long     uc_flags;
+ struct ucontext   *uc_link;
+ stack_t           uc_stack;
+ struct sigcontext uc_mcontext;
+ sigset_t          uc_sigmask;
+} sig_ucontext_t;
 
 class filter_benchmark {
  public:
@@ -355,6 +369,76 @@ class filter_benchmark {
   packet_store *store_;
 };
 
+void crit_err_hdlr(int sig_num, siginfo_t * info, void * ucontext) {
+  sig_ucontext_t * uc = (sig_ucontext_t *)ucontext;
+
+  void * caller_address = (void *) uc->uc_mcontext.eip; // x86 specific
+
+  std::cerr << "signal " << sig_num
+            << " (" << strsignal(sig_num) << "), address is "
+            << info->si_addr << " from " << caller_address
+            << std::endl << std::endl;
+
+  void * array[50];
+  int size = backtrace(array, 50);
+
+  array[1] = caller_address;
+
+  char ** messages = backtrace_symbols(array, size);
+
+  // skip first stack frame (points here)
+  for (int i = 1; i < size && messages != NULL; ++i) {
+    char *mangled_name = 0, *offset_begin = 0, *offset_end = 0;
+
+    // find parantheses and +address offset surrounding mangled name
+    for (char *p = messages[i]; *p; ++p) {
+      if (*p == '(') {
+        mangled_name = p;
+      } else if (*p == '+') {
+        offset_begin = p;
+      } else if (*p == ')') {
+        offset_end = p;
+        break;
+      }
+    }
+
+    // if the line could be processed, attempt to demangle the symbol
+    if (mangled_name && offset_begin && offset_end &&
+        mangled_name < offset_begin) {
+      *mangled_name++ = '\0';
+      *offset_begin++ = '\0';
+      *offset_end++ = '\0';
+
+      int status;
+      char * real_name = abi::__cxa_demangle(mangled_name, 0, 0, &status);
+
+      // if demangling is successful, output the demangled function name
+      if (status == 0) {
+        std::cerr << "[bt]: (" << i << ") " << messages[i] << " : "
+                  << real_name << "+" << offset_begin << offset_end
+                  << std::endl;
+
+      }
+      // otherwise, output the mangled function name
+      else {
+        std::cerr << "[bt]: (" << i << ") " << messages[i] << " : "
+                  << mangled_name << "+" << offset_begin << offset_end
+                  << std::endl;
+      }
+      free(real_name);
+    }
+    // otherwise, print the whole line
+    else {
+      std::cerr << "[bt]: (" << i << ") " << messages[i] << std::endl;
+    }
+  }
+  std::cerr << std::endl;
+
+  free(messages);
+
+  exit(EXIT_FAILURE);
+}
+
 const char* usage =
   "Usage: %s [-b bench-type] [-q query-rate] [-l load-rate] [-p num-packets] [-n num-threads] [-c measure-cpu] query-path\n";
 
@@ -363,6 +447,18 @@ void print_usage(char *exec) {
 }
 
 int main(int argc, char** argv) {
+  struct sigaction sigact;
+
+  sigact.sa_sigaction = crit_err_hdlr;
+  sigact.sa_flags = SA_RESTART | SA_SIGINFO;
+
+  if (sigaction(SIGSEGV, &sigact, (struct sigaction *)NULL) != 0) {
+    fprintf(stderr, "error setting signal handler for %d (%s)\n",
+            SIGSEGV, strsignal(SIGSEGV));
+
+    exit(EXIT_FAILURE);
+  }
+
   int c;
   std::string bench_type = "latency-get";
   uint64_t num_pkts = 60 * 1e6;
