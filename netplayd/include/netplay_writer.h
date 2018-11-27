181e7a8b1d2424fdb6b0a43743f5d77a7cad52e6
John Yang <john.yang20@berkeley.edu>
2018-11-26 19:13:36 -0800

    [netplayd] Replacing packetstore references with multilog instances + commenting

--- netplayd/include/netplay_writer.h
+++ netplayd/include/netplay_writer.h
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <chrono>
 
+#include <confluo_store.h>
 #include <rte_mbuf.h>
 #include <rte_ether.h>
 #include <rte_ip.h>
@@ -13,7 +14,6 @@
 #include <rte_udp.h>
 #include <rte_lpm.h>
 
-#include "packetstore.h"
 #include "tokens.h"
 
 namespace netplay {
@@ -22,28 +22,46 @@ namespace netplay {
 
 template<typename vport_init>
 class netplay_writer {
+ /**
+  * Initialize instance of netplay writer.
+  *
+  * @param core: Number of cores of device
+  * @param vport: Virtual port instance from DPDK/include directory for sending/receiving packets
+  * @param mlog: Confluo atomic multilog instance for packet analysisleldldkdkd
+  */
  public:
-  netplay_writer(int core, dpdk::virtual_port<vport_init>* vport, packet_store::handle* handle) {
+  netplay_writer(int core, dpdk::virtual_port<vport_init>* vport, confluo::atomic_multilog* mlog) {
     rec_pkts_ = 0;
     core_ = core;
     vport_ = vport;
-    handle_ = handle;
+    mlog_ = mlog;
   }
 
+  /**
+   * Begin receiving and forwarding packets to confluo multilog instance
+   */
   void start() {
     struct rte_mbuf* pkts[BATCH_SIZE];
-    
     while (1) {
       uint16_t recv = vport_->recv_pkts(pkts, BATCH_SIZE);
-      handle_->insert_pktburst(pkts, recv);
+      // TODO(john-b-yang): Replace pktburst functionality with atomic multilog
+      // Refer to packetstore code and reimplement with confluo code.
+      // handle_->insert_pktburst(pkts, recv);
+
       rec_pkts_ += recv;
     }
   }
 
+  /**
+   * Return number of cores.
+   */
   int core() {
     return core_;
   }
 
+  /**
+   * Return number of packets received since calling start method.
+   */
   uint64_t rec_pkts() {
     return rec_pkts_;
   }
@@ -58,7 +76,7 @@ class netplay_writer {
   int core_;
   uint64_t rec_pkts_;
   dpdk::virtual_port<vport_init>* vport_;
-  packet_store::handle* handle_;
+  confluo::atomic_multilog* mlog;
 };
 
 }
